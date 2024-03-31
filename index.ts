259f26513fd3e09ca48b2afc97aec2b57c01d10f
Ehtisham86 <ehtisham010101@gmail.com>
2024-03-31 12:50:52 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos = [];
+let condition = true;
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      type: "input",
+      name: "task",
+      message: chalk.yellow("What task would you like to add?"),
+    },
+    {
+      type: "confirm",
+      name: "addAnother",
+      message: chalk.yellow("Do you want to add another task?"),
+    },
+  ]);
+
+  todos.push(addTask.task);
+  condition = addTask.addAnother;
+  console.log(chalk.green(todos));
+}
